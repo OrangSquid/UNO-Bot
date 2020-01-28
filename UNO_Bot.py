dd5253065e20ca768d9717801ae62c02c0061919
BuildTools <unconfigured@null.spigotmc.org>
2020-01-28 21:56:12 +0000

    Add "start", "play_uno" and "join" functionality

--- UNO_Bot.py
+++ UNO_Bot.py
@@ -4,8 +4,6 @@ import UNO_Core
 import json
 from discord.ext import commands
 
-unob = commands.Bot(command_prefix = "!")
-
 # Setting up the logger function for the library
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
@@ -13,16 +11,30 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 handler.setFormatter(logging.Formatter('[%(asctime)s]: %(levelname)s: %(name)s: %(message)s'))
 logger.addHandler(handler)
 
+# TODO: add a way to change the command prefix
+async def prefix(bot, message):
+	return "!"
+
+unob = commands.Bot(command_prefix = prefix)
+
+# Global variables to keep track of games and who's waiting
 CURRENT_GAMES = {}
 WAITING = {}
 
 @unob.command()
 async def play_uno(ctx):
-	WAITING[ctx.guild] = [ctx.author]
-	print(WAITING)
-	await ctx.send("{user} wants to start a game!".format(user = ctx.author.mention))
-	await ctx.send("If anybody wants to join the game, please use !join")
-	await ctx.send("Up to 6 players can join")
+	# Checks if there's already a game waiting to start
+	# If not, make a new one
+	try:
+		WAITING[ctx.guild]
+	except KeyError:
+		WAITING[ctx.guild] = [ctx.author]
+		print(WAITING)
+		await ctx.send("{user} wants to start a game!".format(user = ctx.author.mention))
+		await ctx.send("If anybody wants to join the game, please use !join")
+		await ctx.send("Up to 6 players can join")
+	else:
+		await ctx.send("There's already a game waiting to start")
 
 @unob.command()
 async def join(ctx):
@@ -36,7 +48,13 @@ async def join(ctx):
 
 @unob.command()
 async def start(ctx):
-	pass
+	players = []
+	try:
+		for player in WAITING[ctx.guild]:
+			player_temp = UNO_Core.Player(player)
+			players.append(player_temp)
+	except KeyError:
+		await ctx.send("There's no game to start!")
 
 @unob.command()
 async def stop(ctx):
--- UNO_Core.py
+++ UNO_Core.py
@@ -1,8 +1,27 @@
+import random
 import discord
-from discord.ext import commands
 
-class Game():
-	pass
+DECK = ["red 0",
+		"red 1",
+		"red 1"]
 
-class Player(discord.User):
-	pass
+class Uno():
+	
+	def __init__(self, players):
+		self.drawing_deck = []
+		self.order = random.shuffle(players)
+
+	def play_game(self):
+		pass
+
+class Player():
+	
+	def __init__(self, user):
+		self.deck = []
+		self.user = user
+
+class Card():
+
+	def __init__(self, number, color):
+		self.number = number
+		self.color = color
