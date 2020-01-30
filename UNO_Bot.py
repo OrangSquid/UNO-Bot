02329aa200dc1f3ab00b339a01fea7d385c8a2b5
BuildTools <unconfigured@null.spigotmc.org>
2020-01-30 19:05:03 +0000

    Deck shuffling and giving cards to players now working

--- UNO_Bot.py
+++ UNO_Bot.py
@@ -1,5 +1,6 @@
 import discord
 import logging
+import random
 import UNO_Core
 import json
 from discord.ext import commands
@@ -11,66 +12,78 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 handler.setFormatter(logging.Formatter('[%(asctime)s]: %(levelname)s: %(name)s: %(message)s'))
 logger.addHandler(handler)
 
+
 # TODO: add a way to change the command prefix
 async def prefix(bot, message):
-	return "!"
+    return "!"
 
-unob = commands.Bot(command_prefix = prefix)
 
 # Global variables to keep track of games and who's waiting
-CURRENT_GAMES = {}
 WAITING = {}
+PLAYING = {}
+uno_bot = commands.Bot(command_prefix=prefix)
 
-@unob.command()
+
+@uno_bot.command()
 async def play_uno(ctx):
-	# Checks if there's already a game waiting to start
-	# If not, make a new one
-	try:
-		WAITING[ctx.guild]
-	except KeyError:
-		WAITING[ctx.guild] = [ctx.author]
-		print(WAITING)
-		await ctx.send("{user} wants to start a game!".format(user = ctx.author.mention))
-		await ctx.send("If anybody wants to join the game, please use !join")
-		await ctx.send("Up to 6 players can join")
-	else:
-		await ctx.send("There's already a game waiting to start")
+    # Checks if there's already a game waiting to start
+    # If not, make a new one
+    try:
+        WAITING[ctx.guild]
+    except KeyError:
+        WAITING[ctx.guild] = [ctx.author]
+        await ctx.send("{user} wants to start a game!".format(user=ctx.author.mention))
+        await ctx.send("If anybody wants to join the game, use !join")
+        await ctx.send("Up to 6 players can join")
+    else:
+        await ctx.send("There's already a game waiting to start")
 
-@unob.command()
+
+@uno_bot.command()
 async def join(ctx):
-	try:
-		if ctx.author in WAITING[ctx.guild]:
-			await ctx.send("You are already waiting for the game")
-		else:
-			WAITING[ctx.guild].append(ctx.author)
-	except KeyError:
-		await ctx.send("There are no current games waiting to start")
+    try:
+        if ctx.author in WAITING[ctx.guild]:
+            await ctx.send("You are already waiting for the game")
+        else:
+            WAITING[ctx.guild].append(ctx.author)
+            await ctx.send("{user} has joined the game".format(ctx.user.mention))
+    except KeyError:
+        await ctx.send("There are no current games waiting to start")
 
-@unob.command()
+
+@uno_bot.command()
 async def start(ctx):
-	players = []
-	try:
-		for player in WAITING[ctx.guild]:
-			player_temp = UNO_Core.Player(player)
-			players.append(player_temp)
-	except KeyError:
-		await ctx.send("There's no game to start!")
+    players = []
+    try:
+        for player in WAITING[ctx.guild]:
+            players.append(UNO_Core.Player(player))
+        game = UNO_Core.Uno(players, ctx.guild)
+        PLAYING[ctx.guild] = game
+        game.play_game()
+        PLAYING.pop(ctx.guild)
+    except KeyError:
+        await ctx.send("There's no game to start!")
 
-@unob.command()
+
+@uno_bot.command()
 async def stop(ctx):
-	pass
+    pass
 
-@unob.command()
+
+@uno_bot.command()
 async def settings(ctx):
-	pass
+    pass
 
-@unob.event
+
+@uno_bot.event
 async def on_ready():
-	print("We are ready to roll!")
+    print("We are ready to roll!")
+
 
 def main():
-	with open("Token.txt", "r") as token:
-		unob.run(token.read())
+    with open("Token.txt", "r") as token:
+        uno_bot.run(token.read())
+
 
 if __name__ == "__main__":
-	main()
+    main()
--- UNO_Core.py
+++ UNO_Core.py
@@ -1,27 +1,44 @@
 import random
-import discord
 
-DECK = ["red 0",
-		"red 1",
-		"red 1"]
+DECK = ['wild card', 'wild card', 'wild card', 'wild card', 'wild plus', 'wild plus', 'wild plus', 'wild plus',
+        'red 0', 'red 1', 'red 1', 'red 2', 'red 2', 'red 3', 'red 3', 'red 4', 'red 4', 'red 5', 'red 5', 'red 6',
+        'red 6', 'red 7', 'red 7', 'red 8', 'red 8', 'red 9', 'red 9', 'red skip', 'red skip', 'red reverse',
+        'red reverse', 'red plus', 'red plus', 'blue 0', 'blue 1', 'blue 1', 'blue 2', 'blue 2', 'blue 3', 'blue 3',
+        'blue 4', 'blue 4', 'blue 5', 'blue 5', 'blue 6', 'blue 6', 'blue 7', 'blue 7', 'blue 8', 'blue 8', 'blue 9',
+        'blue 9', 'blue skip', 'blue skip', 'blue reverse', 'blue reverse', 'blue plus', 'blue plus', 'yellow 0',
+        'yellow 1', 'yellow 1', 'yellow 2', 'yellow 2', 'yellow 3', 'yellow 3', 'yellow 4', 'yellow 4', 'yellow 5',
+        'yellow 5', 'yellow 6', 'yellow 6', 'yellow 7', 'yellow 7', 'yellow 8', 'yellow 8', 'yellow 9', 'yellow 9',
+        'yellow skip', 'yellow skip', 'yellow reverse', 'yellow reverse', 'yellow plus', 'yellow plus', 'green 0',
+        'green 1', 'green 1', 'green 2', 'green 2', 'green 3', 'green 3', 'green 4', 'green 4', 'green 5', 'green 5',
+        'green 6', 'green 6', 'green 7', 'green 7', 'green 8', 'green 8', 'green 9', 'green 9', 'green skip',
+        'green skip', 'green reverse', 'green reverse', 'green plus', 'green plus']
 
-class Uno():
-	
-	def __init__(self, players):
-		self.drawing_deck = []
-		self.order = random.shuffle(players)
 
-	def play_game(self):
-		pass
+class Uno:
 
-class Player():
-	
-	def __init__(self, user):
-		self.deck = []
-		self.user = user
+    def __init__(self, players, guild):
+        self.guild = guild
+        self.drawing_deck = random.shuffle(DECK)
+        self.order = random.shuffle(players)
+        self.on_table = self.drawing_deck.pop(0)
+        validate = self.on_table.find
+        while validate("wild") or validate("reverse") or validate("skip") or validate("plus"):
+            self.drawing_deck.append(self.on_table)
+            self.on_table = self.drawing_deck.pop(0)
 
-class Card():
+        for player in self.order:
+            for x in range(0, 6):
+                player.deck.append(self.drawing_deck.pop(0))
 
-	def __init__(self, number, color):
-		self.number = number
-		self.color = color
+    async def play_game(self):
+        for player in self.order:
+            await player.user.send()
+        while len(self.order) != 1:
+            for player in self.order:
+                pass
+
+class Player:
+
+    def __init__(self, user):
+        self.deck = []
+        self.user = user
