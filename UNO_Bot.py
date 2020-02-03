d01ef9b9cc29747c635e2f1d34370546e2899f50
BuildTools <unconfigured@null.spigotmc.org>
2020-02-03 17:44:41 +0000

    Game loop now works

--- UNO_Bot.py
+++ UNO_Bot.py
@@ -1,8 +1,5 @@
-import discord
 import logging
-import random
 import UNO_Core
-import json
 from discord.ext import commands
 
 # Setting up the logger function for the library
@@ -19,8 +16,9 @@ async def prefix(bot, message):
 
 
 # Global variables to keep track of games and who's waiting
-WAITING = {}
-PLAYING = {}
+waiting = {}
+playing = {}
+CARD_EMOJIS = {}
 uno_bot = commands.Bot(command_prefix=prefix)
 
 
@@ -29,9 +27,9 @@ async def play_uno(ctx):
     # Checks if there's already a game waiting to start
     # If not, make a new one
     try:
-        WAITING[ctx.guild]
+        waiting[ctx.guild]
     except KeyError:
-        WAITING[ctx.guild] = [ctx.author]
+        waiting[ctx.guild] = [ctx.author]
         await ctx.send("{user} wants to start a game!".format(user=ctx.author.mention))
         await ctx.send("If anybody wants to join the game, use !join")
         await ctx.send("Up to 6 players can join")
@@ -42,27 +40,31 @@ async def play_uno(ctx):
 @uno_bot.command()
 async def join(ctx):
     try:
-        if ctx.author in WAITING[ctx.guild]:
+        if ctx.author in waiting[ctx.guild]:
             await ctx.send("You are already waiting for the game")
         else:
-            WAITING[ctx.guild].append(ctx.author)
-            await ctx.send("{user} has joined the game".format(ctx.user.mention))
+            waiting[ctx.guild].append(ctx.author)
+            await ctx.send("{} has joined the game".format(ctx.author.mention))
     except KeyError:
         await ctx.send("There are no current games waiting to start")
 
 
 @uno_bot.command()
 async def start(ctx):
-    players = []
-    try:
-        for player in WAITING[ctx.guild]:
-            players.append(UNO_Core.Player(player))
-        game = UNO_Core.Uno(players, ctx.guild)
-        PLAYING[ctx.guild] = game
-        game.play_game()
-        PLAYING.pop(ctx.guild)
-    except KeyError:
-        await ctx.send("There's no game to start!")
+    if len(waiting[ctx.guild]) == 1:
+        return
+    else:
+        players = []
+        try:
+            for player in waiting[ctx.guild]:
+                players.append(UNO_Core.Player(player))
+            game = UNO_Core.Uno(players, CARD_EMOJIS, ctx.channel, uno_bot)
+            await ctx.send("Let's play")
+            playing[ctx.guild] = game
+            await game.play_game()
+            playing.pop(ctx.guild)
+        except KeyError:
+            await ctx.send("There's no game to start!")
 
 
 @uno_bot.command()
@@ -77,6 +79,70 @@ async def settings(ctx):
 
 @uno_bot.event
 async def on_ready():
+    global CARD_EMOJIS
+    CARD_EMOJIS = {"red 0": uno_bot.get_emoji(672954923008131122),
+                   "red 1": uno_bot.get_emoji(672954938409615383),
+                   "red 2": uno_bot.get_emoji(672955019666128946),
+                   "red 3": uno_bot.get_emoji(672955040058834954),
+                   "red 4": uno_bot.get_emoji(672955053639991337),
+                   "red 5": uno_bot.get_emoji(672955063106404411),
+                   "red 6": uno_bot.get_emoji(672955071000215553),
+                   "red 7": uno_bot.get_emoji(672955078142853141),
+                   "red 8": uno_bot.get_emoji(672955085919092748),
+                   "red 9": uno_bot.get_emoji(672955093489811467),
+                   "red plus": uno_bot.get_emoji(672955105590509595),
+                   "red reverse": uno_bot.get_emoji(672955116407619595),
+                   "red skip": uno_bot.get_emoji(672955128281563146),
+                   "yellow 0": uno_bot.get_emoji(672955142232080404),
+                   "yellow 1": uno_bot.get_emoji(672955153032282122),
+                   "yellow 2": uno_bot.get_emoji(672955162494500864),
+                   "yellow 3": uno_bot.get_emoji(672955172355440651),
+                   "yellow 4": uno_bot.get_emoji(672955181729579008),
+                   "yellow 5": uno_bot.get_emoji(672955193968820235),
+                   "yellow 6": uno_bot.get_emoji(672955205025005572),
+                   "yellow 7": uno_bot.get_emoji(672955215166701598),
+                   "yellow 8": uno_bot.get_emoji(672955225396477973),
+                   "yellow 9": uno_bot.get_emoji(672955235005759508),
+                   "yellow plus": uno_bot.get_emoji(672955248784179208),
+                   "yellow reverse": uno_bot.get_emoji(672955258598850589),
+                   "yellow skip": uno_bot.get_emoji(672955269512167454),
+                   "green 0": uno_bot.get_emoji(672955280916611084),
+                   "green 1": uno_bot.get_emoji(672955290123239445),
+                   "green 2": uno_bot.get_emoji(672955297974714369),
+                   "green 3": uno_bot.get_emoji(672955305839296562),
+                   "green 4": uno_bot.get_emoji(672955311648276500),
+                   "green 5": uno_bot.get_emoji(672955324864528385),
+                   "green 6": uno_bot.get_emoji(672955334033276960),
+                   "green 7": uno_bot.get_emoji(672955343843622923),
+                   "green 8": uno_bot.get_emoji(672955351926177812),
+                   "green 9": uno_bot.get_emoji(672955360570638369),
+                   "green plus": uno_bot.get_emoji(672955374319566877),
+                   "green reverse": uno_bot.get_emoji(672955383999889448),
+                   "green skip": uno_bot.get_emoji(672955391277137931),
+                   "blue 0": uno_bot.get_emoji(672955401897246721),
+                   "blue 1": uno_bot.get_emoji(672955411887816714),
+                   "blue 2": uno_bot.get_emoji(672955419764719626),
+                   "blue 3": uno_bot.get_emoji(672955428727947284),
+                   "blue 4": uno_bot.get_emoji(672955436139544640),
+                   "blue 5": uno_bot.get_emoji(672955445169881138),
+                   "blue 6": uno_bot.get_emoji(672955453335928870),
+                   "blue 7": uno_bot.get_emoji(672955460701388830),
+                   "blue 8": uno_bot.get_emoji(672955468284559380),
+                   "blue 9": uno_bot.get_emoji(672955477063237653),
+                   "blue plus": uno_bot.get_emoji(672955485066100746),
+                   "blue reverse": uno_bot.get_emoji(672955984477552653),
+                   "blue skip": uno_bot.get_emoji(672955996041248780),
+                   "wild card": uno_bot.get_emoji(672956006158041126),
+                   "wild plus": uno_bot.get_emoji(672956016542875681),
+                   "red wild card": uno_bot.get_emoji(673909975764041749),
+                   "red wild plus": uno_bot.get_emoji(673909988846206976),
+                   "yellow wild card": uno_bot.get_emoji(673910001475125250),
+                   "yellow wild plus": uno_bot.get_emoji(673910013437149194),
+                   "green wild card": uno_bot.get_emoji(673909955375398928),
+                   "green wild plus": uno_bot.get_emoji(673909964263391243),
+                   "blue wild card": uno_bot.get_emoji(673909927131217971),
+                   "blue wild plus": uno_bot.get_emoji(673909940867301377)
+                   }
     print("We are ready to roll!")
 
 
--- UNO_Core.py
+++ UNO_Core.py
@@ -16,26 +16,130 @@ DECK = ['wild card', 'wild card', 'wild card', 'wild card', 'wild plus', 'wild p
 
 class Uno:
 
-    def __init__(self, players, guild):
-        self.guild = guild
-        self.drawing_deck = random.shuffle(DECK)
-        self.order = random.shuffle(players)
+    def __init__(self, players, card_emojis, channel, bot):
+        # Variables to be used later
+        self.waiting_for = None       # The current player's turn
+        self.playing_card = None      # Holds the card to be played temporarily for checking
+        self.color_change = None      # Holds the color information for change if wild card is used
+        self.skip = False             # Tells if next player's turn is skipped or not
+        self.draw = 0                 # Tells how many cards the next player must draw
+        self.played_cards = []
+        self.CARD_EMOJIS = card_emojis
+        self.channel = channel
+        self.bot = bot
+        self.drawing_deck = random.sample(DECK, k=len(DECK))
+        self.order = random.sample(players, k=len(players))
         self.on_table = self.drawing_deck.pop(0)
-        validate = self.on_table.find
-        while validate("wild") or validate("reverse") or validate("skip") or validate("plus"):
+        # Validation of the selected card on table
+        # Wild, reverse, skip and +2 cards are invalid to start a game of UNO
+        validate_back = self.on_table.endswith
+        validate_front = self.on_table.startswith
+        while validate_front("wild") or validate_back("reverse") or validate_back("skip") or validate_back("plus"):
             self.drawing_deck.append(self.on_table)
             self.on_table = self.drawing_deck.pop(0)
 
         for player in self.order:
-            for x in range(0, 6):
+            for x in range(0, 7):
                 player.deck.append(self.drawing_deck.pop(0))
 
     async def play_game(self):
+        await self.channel.send("The order is as follows: ")
         for player in self.order:
-            await player.user.send()
+            await self.channel.send(player.user.mention)
+            await player.user.send("This is your deck")
+            await player.user.send(self.deck_to_emoji(player))
+        await self.channel.send("Card on the table:")
+        await self.channel.send(self.CARD_EMOJIS[self.on_table])
+        pointer = 0
         while len(self.order) != 1:
-            for player in self.order:
-                pass
+            player = self.order[pointer]
+            if self.skip:
+                await self.channel.send("{.user.mention}'s turn is skipped!".format(player))
+                self.skip = False
+                continue
+            if self.draw:
+                await self.channel.senf("{.user.mention} drew {} cards and their turn is skipped".format(player, self.draw))
+                for x in range(self.draw):
+                    player.deck.append(self.drawing_deck.pop(0))
+                self.draw = 0
+                continue
+            await self.channel.send("Your turn: {.user.mention}".format(player))
+            self.waiting_for = player
+            # Loop for the player's turn
+            while True:
+                await self.bot.wait_for("message", check=self.check_playing_card)
+                # Makes the card on the table a list for easier checking with indexes
+                list_on_table = self.on_table.split()
+                # Check card sent in DMs:
+                # Wild Card
+                if self.playing_card.startswith("wild"):
+                    player.deck.remove(self.playing_card)
+                    await player.user.send("Choose your color (red, yellow, green, blue)")
+                    await self.bot.wait_for("message", check=self.check_wild_card_color)
+                    await self.channel.send("{.user.mention} played a:".format(player))
+                    await self.channel.send(self.CARD_EMOJIS["{} {}".format(self.color_change, self.playing_card)])
+                    self.on_table = "{} wild".format(self.color_change)
+                    self.color_change = None
+                    if self.playing_card.endswith("plus"):
+                        self.draw = 4
+                    self.playing_card = None
+                # Normal Card
+                elif self.playing_card.startswith(list_on_table[0]) or self.playing_card.endswith(list_on_table[1]):
+                    player.deck.remove(self.playing_card)
+                    await self.channel.send("{.user.mention} played a:".format(player))
+                    await self.channel.send(self.CARD_EMOJIS[self.playing_card])
+                    self.on_table = self.playing_card
+                    # Skip Card
+                    if self.playing_card.endswith("skip"):
+                        self.skip = True
+                    # 2+ Card
+                    elif self.playing_card.endswith("plus"):
+                        self.draw = 2
+                    # Reverse Card
+                    elif self.playing_card.endswith("reverse"):
+                        self.order.reverse()
+                        if len(self.order) == 2:
+                            pointer -= 1
+                        else:
+                            pointer = abs(pointer - len(self.order))
+                    self.played_cards.append(self.played_card)
+                    self.playing_card = None
+                # Draw a Card
+                elif self.playing_card == "draw":
+                    player.deck.append(self.drawing_deck.pop(0))
+                    await self.channel.send("{.user.mention} drew a card".format(player))
+                # Invalid Card
+                else:
+                    await player.user.send("That card cannot be played!")
+                    continue
+                await player.user.send(self.deck_to_emoji(player))
+                break
+            if pointer == len(self.order) - 1:
+                pointer = 0
+            else:
+                pointer += 1
+
+    async def played_card(self):
+        pass
+
+    def check_playing_card(self, message):
+        content = message.content
+        if (message.author == self.waiting_for.user) and (content in self.waiting_for.deck or content == "draw"):
+            self.playing_card = content
+            return True
+
+    def check_wild_card_color(self, message):
+        colors = ["red", "yellow", "green", "blue"]
+        if message.author == self.waiting_for.user and message.content in colors:
+            self.color_change = message.content
+            return True
+
+    def deck_to_emoji(self, player):
+        to_send = ""
+        for card in player.deck:
+            to_send += str(self.CARD_EMOJIS[card])
+        return to_send
+
 
 class Player:
 
